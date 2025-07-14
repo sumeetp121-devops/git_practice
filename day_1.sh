83001945f269ac480111a126ec5e63438b5543ea
Sumeet Patel <sumeet.prac0811@gmail.com>
2025-07-14 17:45:02 +0530

    added a script file

---
+++ day_1.sh
@@ -0,0 +1,7 @@
+# this script will print the current date and time
+echo "Current date and time: $(date)"
+# and then list the files in the current directory
+echo "Files in the current directory:"
+ls -l
+# finally, it will print a message indicating the script has completed
+echo "Script execution completed."
